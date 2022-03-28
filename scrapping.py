c53ba891e9c610891de9246c3f1a99cfd37db143
Hugo Hernany <hugosc@ciandt.com>
2022-03-28 19:52:43 -0400

    TO-DO

--- scrapping.py
+++ scrapping.py
@@ -7,13 +7,14 @@ import requests
 import xlsxwriter
 
 
-# Arquivo final de XLS
+# XLSX file
 workbook = xlsxwriter.Workbook('fiis.xlsx')
-worksheet = workbook.add_worksheet() # Adiciona uma aba
+worksheet = workbook.add_worksheet()
 
 sheetRow = 1
 currency_format = workbook.add_format({'num_format': '[$R$ -416]#,##0.00'})
 
+# Columns
 writers = {
     "A1": "ATIVO",
     "B1": "CNPJ",
@@ -35,30 +36,38 @@ writers = {
     "R1": "QTD DE ATIVOS",
 }
 
-# Use the worksheet object to write
-# data via the write() method.
+# Use the worksheet object to write data via the write() method.
 for key, val in writers.items():
     worksheet.write(key, val)
 
-# Ler base de dados com os FIIs
+# Read FIIs code from database
 data = []
 with open('fonte.csv', "r") as csvfile:
-    reader = csv.reader(csvfile, delimiter=";")
+    reader = csv.reader(csvfile, delimiter=",")
     for row in reader:
         data.append(row[0])
 
-# Objetos com as informações
+# Objects with FIIs informations
 treeSI = Any
 treeI10 = Any
 treeFE = Any
 
-# Lê as informações e salva no Excel
+# Replace last ocurrence
+def replace_right(source, target, replacement, replacements=1):
+    return replacement.join(source.rsplit(target, replacements))
+
+# Read and save informations
 def processData(fii):
     sheetColumn = 0
     worksheet.write(sheetRow, sheetColumn, fii)
     sheetColumn += 1
 
     qtdAtivos = treeFE.xpath('(//*[@id="fund-actives-chart-info-wrapper"]/span[1])/text()') # QTD DE ATIVOS
+    vlrPatrimonialPorCota = treeSI.xpath('//*[@id="main-2"]/div[2]/div[5]/div/div[1]/div/div[1]/strong/text()')[0].replace("R$","").replace(" ","").replace(",", ".") # VALOR PATRIMONIAL POR COTA
+    vlrPatrimonio = treeSI.xpath('//*[@id="main-2"]/div[2]/div[5]/div/div[1]/div/div[2]/span[2]/text()')[0].replace("R$","").replace(" ","").replace(",", ".") # VALOR DO PATRIMONIO
+    vlrMercado = treeSI.xpath('//*[@id="main-2"]/div[2]/div[5]/div/div[2]/div/div[2]/span[2]/text()')[0].replace("R$","").replace(" ","").replace(",", ".") # VALOR DE MERCADO
+    lqdzDiaria = treeSI.xpath('//*[@id="main-2"]/div[2]/div[6]/div/div/div[3]/div/div/div/strong/text()')[0].replace("R$","").replace(" ","").replace(",", ".") # LIQUIDEZ MEDIA 30 DIAS
+
     # STATUS INVEST
     dadosFII = [
         treeSI.xpath('//*[@id="fund-section"]/div/div/div[2]/div/div[1]/div/div/strong/text()')[0], # CNPJ
@@ -69,11 +78,10 @@ def processData(fii):
         treeSI.xpath('//*[@id="main-2"]/div[2]/div[5]/div/div[6]/div/div[1]/strong/text()')[0], # TOTAL DE COTISTAS
         treeSI.xpath('//*[@id="main-2"]/div[2]/div[5]/div/div[6]/div/div[2]/span[2]/text()')[0], # TOTAL DE COTAS
 
-        # TO-DO - TROCAR O ULTIMO PONTO DO VALOR POR VÍRGULA, PARA SEGUIR O PADRÃO.
-        treeSI.xpath('//*[@id="main-2"]/div[2]/div[5]/div/div[1]/div/div[1]/strong/text()')[0].replace("R$","").replace(" ",""), # VALOR PATRIMONIAL POR COTA
-        treeSI.xpath('//*[@id="main-2"]/div[2]/div[5]/div/div[1]/div/div[2]/span[2]/text()')[0].replace("R$","").replace(" ",""), # VALOR DO PATRIMONIO
-        treeSI.xpath('//*[@id="main-2"]/div[2]/div[5]/div/div[2]/div/div[2]/span[2]/text()')[0].replace("R$","").replace(" ",""), # VALOR DE MERCADO
-        treeSI.xpath('//*[@id="main-2"]/div[2]/div[6]/div/div/div[3]/div/div/div/strong/text()')[0].replace("R$","").replace(" ","").replace(",","."), # LIQUIDEZ MEDIA 30 DIAS
+        replace_right(vlrPatrimonialPorCota, ".", ","),
+        replace_right(vlrPatrimonio, ".", ","),
+        replace_right(vlrMercado, ".", ","),
+        replace_right(lqdzDiaria, ".", ","),
         treeSI.xpath('//*[@id="dy-info"]/div/div[1]/strong/text()')[0], # ULTIMO RENDIMENTO
 
         # INVESTIDOR 10
@@ -83,13 +91,15 @@ def processData(fii):
         # FUNDS EXPLORER
         treeFE.xpath('(//span[@class="indicator-value"])[1]/text()')[0].replace("\n","").replace(" ",""), # QTD. DE NEGOCIAÇÕES DIÁRIAS
         treeFE.xpath('(//span[@class="description"])[12]/text()')[0].replace("\n","").replace(" ",""), # SEGMENTO
-        re.sub("[^0-9]", "", qtdAtivos[0]),
-    ] 
-    #vacancia = ... PENDENTE
+        re.sub("[^0-9]", "", qtdAtivos[0])
+
+        # TO-DO - OBTER OS DADOS DA VACANCIA
+    ]
     
-    # Gravação do XLSX
+    # Write XLSX file
     for dado in dadosFII:
-        if sheetColumn >= 8 and sheetColumn <= 11:
+        # TO-DO - CONVERTER TODOS OS CAMPOS DE NÚMERO PARA TIPO DE NÚMERO, POIS ESTÁ INDO COMO TEXTO.
+        if sheetColumn >= 8 and sheetColumn <= 12:
             worksheet.write(sheetRow, sheetColumn, dado, currency_format)
         else:
             worksheet.write(sheetRow, sheetColumn, dado)
@@ -109,7 +119,7 @@ for fii in data:
     processData(fii)
     sheetRow +=1
 
-# Aplicar filtro
+# Apply filter
 lastRow = "R"+str(sheetRow)
 range = "A1:" + lastRow
 worksheet.autofilter(range)
